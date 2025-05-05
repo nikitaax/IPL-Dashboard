8d444890aea9bbdb9be607738092222951710ba5
nikitaax <nikkitanandankar@gmail.com>
2025-05-05 18:33:42 +0530

    scraping update

--- src/app/api/scrapers/fixturesScraping.tsx
+++ src/app/api/scrapers/fixturesScraping.tsx
@@ -11,6 +11,8 @@ export async function scrapeFixtures() {
   const browser = await puppeteer.launch({
     headless: true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    executablePath:
+      process.env.PUPPETEER_EXECUTABLE_PATH || "/usr/bin/google-chrome-stable",
   });
 
   const page = await browser.newPage();
