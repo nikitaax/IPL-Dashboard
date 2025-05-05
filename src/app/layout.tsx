3810fdacc66898db0745fcc937d046377245b30e
nikitaax <nikkitanandankar@gmail.com>
2025-05-05 17:18:46 +0530

    added fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,19 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "./components/navbar";
 import "./assets/globals.css";
 
-export const metadata = {
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
   title: "IPL Dashboard",
   description: "Get the latest updates about IPL 2025",
 };
@@ -13,7 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <div className="bg-[url('/background.png')] bg-cover bg-center min-h-screen ">
           <Navbar />
           {children}
