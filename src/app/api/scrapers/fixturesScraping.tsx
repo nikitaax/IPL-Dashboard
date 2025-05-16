a114503b052040f7c6e40eb4c9a5791f9cd44395
nikitaax <nikkitanandankar@gmail.com>
2025-05-16 20:25:22 +0530

    fix

--- src/app/api/scrapers/fixturesScraping.tsx
+++ src/app/api/scrapers/fixturesScraping.tsx
@@ -45,7 +45,7 @@ export async function scrapeFixtures() {
     "Punjab Kings": "PBKS",
     "Lucknow Super Giants": "LSG",
     "Gujarat Titans": "GT",
-    "TBD ": "TBD",
+    TBD: "TBD",
   };
 
   const TBD_IMAGE_URL =
